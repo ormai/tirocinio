5d4e3023a162b5fd80d9cc02289316e898c31f1c
Mario D'Andrea <goffredo2004@gmail.com>
2026-05-08 19:06:32 +0200

    Document further `class Field`

--- src/lib/form/field.svelte.ts
+++ src/lib/form/field.svelte.ts
@@ -1,36 +1,89 @@
 import type { LocalizedString } from '@inlang/paraglide-js';
 
 /**
- * Given an input field validates it, returning `false` if valid or a feedback
- * string explaining why it is invalid and giving advice for a fix.
+ * Given an input field, the Validator validates it, returning `false` if valid or a feedback
+ * string explaining why it is invalid and giving advice for a fix, to be displayed in UI.
  */
 type Validator = (input: HTMLInputElement) => LocalizedString | false;
 
+/**
+ * A valid password has at least
+ * - one uppercase letter (A-Z)
+ * - one lowercase letter (a-z)
+ * - one digit (0-9)
+ * - one symbol among !#$%&"'()*+,-./\:;<=>?@[]^_`|~
+ * - ten characters
+ */
 export const passwordRegExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!#$%&"'()*+,\-./:;<=>?@[\\\]^_`|~]).{10,}$/;
 
 /**
  * Interaction logic for an input field in a form.
  */
 export class Field {
-  private input = $state<HTMLInputElement>();
+  private input?: HTMLInputElement = $state();
+
+  /**
+   * Whether the field is valid or not according to the constraints defined in its constructor.
+   */
+  get valid(): boolean {
+    return this._error === '';
+  }
+
   private _error: string = $state('');
 
-  private _dirty: boolean = $state(false);
-
-  get error() {
+  /**
+   * @returns {string} feedback message about the current invalid state of the field as **reactive
+   * state**. The empty string `''` stands for a valid field.
+   */
+  get error(): string {
     return this._error;
   }
 
-  get dirty() {
+  private _dirty: boolean = $state(false);
+
+  /**
+   * @returns {boolean} Wether the field was _touched_ interactively by the user. Returns **reactive
+   * state**.
+   */
+  get dirty(): boolean {
     return this._dirty;
+  }
+
+  private _value = $state('');
+
+  /**
+   * The content of the `value` property of {@link HTMLInputElement} as **reactive state** that is
+   * updated when the `input` event fires on the element.
+   */
+  get value(): string {
+    return this._value;
+  }
+
+  /**
+   * Update the `value` property of the inner {@link HTMLInputElement}.
+   */
+  set value(value: string | undefined | null) {
+    if (this.input) {
+      this.input.value = value ?? '';
+    } else {
+      console.warn('Attempt to mutate a Field that is not attached to the DOM');
+    }
   }
 
   /**
    * @param validators {Validator[]} used to validate the value of the field
    * @param onClearServerErrors {() => void} callback to clear server errors on input, defaults to no-op.
    */
-  constructor(private validators: Validator[] = [], private onClearServerErrors: () => void = () => {}) {}
+  constructor(
+    private readonly validators: Validator[] = [],
+    private readonly onClearServerErrors: () => void = () => {},
+  ) {}
 
+  /**
+   * Applies the constrains passed in the constructor, **in order**, stopping at the first one that
+   * results to be violated. This method will update the feedback message and mutate the DOM to
+   * display it.
+   */
   validate() {
     if (!this.input) return;
     for (const validator of this.validators) {
@@ -71,6 +124,9 @@ export class Field {
     this.input = input;
 
     const onInput = () => {
+      if (this.input) {
+        this._value = this.input.value;
+      }
       this.onClearServerErrors();
       if (this._dirty) {
         this.validate();
@@ -88,19 +144,33 @@ export class Field {
     return () => {
       input.removeEventListener('input', onInput);
       input.removeEventListener('blur', onBlur);
-      this._error = '';
-      this._dirty = false;
+      this.reset();
       this.input = undefined;
     };
   };
 
-  /** Validates and marks dirty before submit. Used in enhance. */
+  /**
+  Validates and marks dirty before submit. Used on the `onSubmit` callback passed to
+   * Svelte's `enhance`.
+   */
   touch() {
     this._dirty = true;
     this.validate();
   }
 
-  get valid(): boolean {
-    return this._error === '';
+  /**
+   * Flushes the validation state. This action is the inverse & opposite of [this.touch].
+   */
+  reset() {
+    this._error = '';
+    this._dirty = false;
+  }
+
+  /**
+   * Flushes the state of the field and sets an optional value.
+   */
+  resetTo(value: string | undefined | null = '') {
+    this.value = value ?? '';
+    this.reset();
   }
 }
