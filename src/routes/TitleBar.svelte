e49596a940a9ba17b651f56795822246e75075a7
Mario D'Andrea <goffredo2004@gmail.com>
2026-06-04 14:40:22 +0200

    Add optional "go back" action in the `TitleBar`

--- src/routes/TitleBar.svelte
+++ src/routes/TitleBar.svelte
@@ -1,28 +1,40 @@
 <!-- @component title bar at the top of the page. Integrates with the {@link SideBar}. -->
 
 <script lang="ts">
+  import { resolve } from '$app/paths';
   import { page } from '$app/state';
+  import type { RouteId } from '$app/types';
   import { m } from '$lib/paraglide/messages';
   import { tooltip } from '$lib/tooltip.svelte.js';
-  import { Menu } from '@lucide/svelte';
+  import { ArrowLeft, Menu } from '@lucide/svelte';
   import { sideBar } from './SideBar.svelte';
 
-  let { title }: { title: string } = $props();
+  let { title, backLocation }: { title: string; backLocation?: RouteId } = $props();
 </script>
 
 <svelte:head><title>{title} - {page.data.appName}</title></svelte:head>
 
 <header>
-  {#if sideBar.mobile && page.data.user.role === 'admin'}
-    <button
-      class="tertiary"
-      style="width: initial"
-      onclick={() => sideBar.collapsed = !sideBar.collapsed}
-      aria-label={m.sidebar_expand()}
-      {@attach tooltip({ content: m.sidebar_expand(), placement: 'right' })}
+  {#if backLocation != null}
+    <a
+      // https://github.com/sveltejs/kit/issues/15536
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      href={resolve(backLocation as any)}
+      class="button"
+      {@attach tooltip({ content: m.nav_back(), placement: 'right' })}
     >
-      <Menu />
-    </button>
+      <ArrowLeft />
+    </a>
+  {:else}
+    {#if sideBar.mobile && page.data.user.role === 'admin'}
+      <button
+        class="tertiary"
+        onclick={() => sideBar.collapsed = !sideBar.collapsed}
+        {@attach tooltip({ content: m.sidebar_expand(), placement: 'right' })}
+      >
+        <Menu />
+      </button>
+    {/if}
   {/if}
   <h1>{title}</h1>
 </header>
